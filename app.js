85336d9deb4513b1f740bafd77a236d7614de0b5
Danny <dxpbusiness@gmail.com>
2022-08-18 04:56:39 -0500

    Applied HTML CSS and Javacsript wiring

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 // Setting up the gameBoard module
 let gameBoardModule = (() => {
-    let gameBoard = {};
-    return {};
+    let gameBoard = ["X"];
+    return {gameBoard};
 })();
 
 // Setting up the displayController module
 let displayControllerModule = (() => {
-    let getPlayerName = () => {console.log("testing private function call inside of a module object.....")};
-    return (testF);
+    const makeMove = document.querySelectorAll(".game-board-button");
+
+    // Start indexing and looping through each button node
+    let index = 0;
+    makeMove.forEach(makeMoves => {
+        makeMoves.dataset.linkedButton = index;
+        makeMoves.addEventListener("click", renderArrayToScreen);
+
+        function renderArrayToScreen() {
+            const gridBoxes = document.querySelectorAll(".grid-box");
+
+            // Start indexing and looping through each grid box node
+            let index = 0;
+            gridBoxes.forEach(gridBox => {
+                gridBox.dataset.linkedButton = index;
+
+                // Render clicked play on the correct grid box and display
+                if (gridBox.getAttribute("data-linked-button") == makeMoves.getAttribute("data-linked-button")) {
+                    gridBox.textContent = gameBoardModule.gameBoard[gameBoardModule.gameBoard.length -1];
+                    console.log("show me my makeMoves linked button value...", makeMoves.dataset.linkedButton);
+                    console.log("show me my gridBox linked button value...", gridBox.dataset.linkedButton);
+                }
+                index++;
+            })
+        }
+        index++
+    })
 })();
 
 // Setting up the player factory function
@@ -17,5 +42,8 @@ let createPlayer = (playerName, playerNumber, assignedXO) => {
     return {getPlayerName, playerName, playerNumber, assignedXO};
 };
 
+
+
 let Justin = createPlayer("Justin", 1, "X");
 let James = createPlayer("James", 2, "O");
+renderArrayToScreenModule;
