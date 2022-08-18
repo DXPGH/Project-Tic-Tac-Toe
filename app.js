7bafc3739b48c054288a55f9e9bf63660dda0e2a
Danny <dxpbusiness@gmail.com>
2022-08-18 03:06:03 -0500

    Applied initial modules and factory functions

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 // Setting up the gameBoard module
-let gameBoardModule = (function() {
-    let gameBoard
-})
+let gameBoardModule = (() => {
+    let gameBoard = {};
+    return {};
+})();
+
+// Setting up the displayController module
+let displayControllerModule = (() => {
+    let getPlayerName = () => {console.log("testing private function call inside of a module object.....")};
+    return (testF);
+})();
+
+// Setting up the player factory function
+let createPlayer = (playerName, playerNumber, assignedXO) => {
+    let getPlayerName = () => {playerName;
+    console.log("This is the name of player " + playerNumber + "....." + playerName);}
+    return {getPlayerName, playerName, playerNumber, assignedXO};
+};
+
+let Justin = createPlayer("Justin", 1, "X");
+let James = createPlayer("James", 2, "O");
